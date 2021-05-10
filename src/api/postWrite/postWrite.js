b615fe6e771dba20351ec3f7401ed8146c79e04f
limyoutaek <atrlkwqr@gmail.com>
2021-05-10 16:57:53 +0900

    add fileserver

--- src/api/postWrite/postWrite.js
+++ src/api/postWrite/postWrite.js
@@ -1,40 +1,46 @@
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
+import {isAuthenticated} from "../../middleware";
 
 const prisma = new PrismaClient()
 
 const writePost = {
-  Mutation: {
-    writePost: async(_, args, {request}) => {
+    Mutation: {
+        writePost: async (_, args, {request}) => {
 
-      try{
-        const{
-          title,
-          contents
-        } = args;
+            try {
+                const isAuth = isAuthenticated(request);
 
-        if(request.user.id !== null){
-          const id = request.user.id;
-          await prisma.post.create({
-            data: {
-              id,
-              title,
-              contents
-            },
-          })
+                if (isAuth === true) {
+
+                    const {title, contents} = args;
+
+                    if (request.user.id !== null) {
+                        const id = request.user.id;
+                        const Post = await prisma
+                            .post
+                            .create({
+                                data: {
+                                    id,
+                                    title,
+                                    contents
+                                }
+                            })
+
+                        return {ok: true, postId: Post.postId}
+                    } else {
+                        return {ok: false, postId: null}
+                    }
+
+                } else {
+                    return {ok: false, postId: null}
+                }
+
+            } catch (err) {
+                console.log(err)
+                return {ok: false, postId: null}
+            }
         }
-        else {
-          false;
-        }
-
-
-        return true;
-
-      } catch(err){
-        console.log(err)
-        return false;
-      }
     }
-  }
 };
 
 export default writePost;
